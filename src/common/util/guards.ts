ded86f48d008bd36ce85f4eb9794b002c0ad6ee2
synek <rory@rory.bio>
2021-04-21 06:30:52 +0100

    feat: improve type-guards and add isChainRef

--- src/common/util/guards.ts
+++ src/common/util/guards.ts
@@ -2,10 +2,21 @@
  * Type Guards
  */
 
-import {CitationRef, FigureRef, ParagraphItem, ParagraphRef, ResourceRef, SectionItem} from "src/features/paperPicker/paperPickerSlice"
+import {CitationRef, FigureRef, ParagraphRef, RefChain, RelatedResourceRef, ResourceRef} from "src/features/paperPicker/paperPickerSlice"
 
 
-export const isText = (item: ParagraphItem): item is string => typeof item == "string"
-export const isCitationRef = (item: ParagraphItem | ResourceRef): item is CitationRef => (item as ResourceRef).kind === "citation"
-export const isParagraphRef = (item: ResourceRef): item is ParagraphRef => item.kind == 'paragraph'
-export const isFigureRef = (item: ResourceRef): item is FigureRef => item.kind == 'figure'
+export const isText = (item: any): item is string => typeof item === "string"
+export const isResourceRef = (item: any): item is ResourceRef => typeof item === "object" && "kind" in item && "id" in item
+
+export const isCitationRef = (item: any): item is CitationRef => isResourceRef(item) && item.kind === "citation"
+export const isParagraphRef = (item: any): item is ParagraphRef => isResourceRef(item) && item.kind === 'paragraph'
+export const isFigureRef = (item: any): item is FigureRef => isResourceRef(item) && item.kind === 'figure'
+
+export const isRelatedResourceRef = (item: any): item is RelatedResourceRef => isFigureRef(item) || isCitationRef(item)
+export const isRefChain = <T extends ResourceRef>(item: any): item is RefChain<T> => (
+	Array.isArray(item) && ((
+		item.length === 0
+	) || (
+		'id' in item[0] && 'kind' in item[0]
+	))
+)
